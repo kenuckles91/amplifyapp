39c639b71c0d1fd4650c80034c46ff1738b4304b
captain kenuckles <ianvh@hotmail.co.za>
2021-11-29 23:39:22 +0200

    V2.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+<html> 
+  <head>
+    <title>Hello World</title> 
+  </head>
+ 
+  <body> 
+    <h1>Hello World</h1> 
+    <h1>Hello World</h1> 
+    <h2>Hello World</h2> 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+  </body>
+</html>
